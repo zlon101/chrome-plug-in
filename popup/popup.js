c1e784a1c622b45758e43f6c27428d85f6bd33f7
erik <czlong607@gmail.com>
2024-12-24 09:41:47 +0800

    feat: 修改UI

--- popup/popup.js
+++ popup/popup.js
@@ -2,4 +2,4 @@ import { onParseCatalog, openSearchBox } from '../render-page/normal/message.js'
 
 // 解析目录
 document.querySelector('.parse_catalog').onclick = onParseCatalog;
-openSearchBox()
+document.querySelector('.popup_search').onclick = openSearchBox;
